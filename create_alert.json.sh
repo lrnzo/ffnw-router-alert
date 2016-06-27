a3ba8b7c096641d3e49e1f4e58bcc2ab04d03d50
lrnzo <lrnzo@posteo.de>
2016-06-27 16:07:22 +0200

    geändert:       create_alert.json.sh

--- create_alert.json.sh
+++ create_alert.json.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+#aus den emailadressen in ./subscribers wird die alert.json erstellt. dies geschieht per cronjob. siehe 'crontab -l' für zB das eingestellte Intervall
 jq --slurpfile address subscribers '.nodes[]|select(.nodeinfo.owner.contact|tostring|[.]|inside($address))|{nodeid: .nodeinfo.node_id, alertme: true}' nodes.json-lastseen | jq -s . > alert.json
